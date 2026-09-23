9e218c899376c6978f6ab5dc8ddd407810648ad1
Alex Farmer <yhs7ar@virginia.edu>
2026-09-23 08:05:51 -0400

    Made a yolo26s model trained on the images in 720p for 30 epochs, untested

--- TrainDogPose_2kpt.py
+++ TrainDogPose_2kpt.py
@@ -1,6 +1,10 @@
+import os
 import torch
 
-# Keep this for AMD Windows GPU
+# Prevent VRAM fragmentation
+os.environ["PYTORCH_ALLOC_CONF"] = "expandable_segments:True"
+
+# Bypass MIOpen runtime kernel compilation on AMD Windows
 torch.backends.cudnn.enabled = False
 
 from ultralytics import YOLO
@@ -11,16 +15,18 @@ def main():
     # Load base pretrained model
     model = YOLO("yolo26s-pose.pt")
 
-    # Train on your new 2-keypoint dataset
+    # Train on your 2-keypoint dataset
     results = model.train(
         data=r"I:\Capstone\Dog-Diving-Scoring\dog-pose-2kpt\dog-pose-2kpt.yaml",
-        epochs=50,
-        imgsz=1920,
+        epochs=30,
+        imgsz=1280,
+        batch=8,
+        workers=2,
         device=0,
-        batch=4,
         project=r"I:\Capstone\Dog-Diving-Scoring\trained_models",
-        name="dog_pose_2kpt_50epochs_1080p_basedata",
-        exist_ok=True
+        name="dog_pose_s_2kpt_30epochs_720p_basedata",
+        exist_ok=True,
+        cache=True
     )
 
 if __name__ == '__main__':
